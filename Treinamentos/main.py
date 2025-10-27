4dc3b00ae2aee6ac4b1a0d012693040e04252dd3
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2025-10-27 19:57:11 -0300

    Ajustar main

--- Treinamentos/main.py
+++ Treinamentos/main.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import yaml
-from src.data_processing import criar_df_dict
-from src.train import treinar_produto, treinar_produto_optuna
+from sklearn.model_selection import train_test_split
+from src.train import treinar, treinar_optnua
+from src.utils import carregar_dados_dengue_de_release
 
 #==============================INPUT==================================================
 # Seleciona experimento
 yaml_file = "xgb_grid_1"
-file_path = "Modelos/Experimentos/" + yaml_file + ".yaml"
+file_path = "Treinamentos/Experimentos/" + yaml_file + ".yaml"
 
-# Define data de início e meses de previsão (opcional)
-data_inicio = pd.to_datetime('2024-10-31')
-num_meses_previsao = 6
+# Define o target (NÃO alterar)
+target = 'RISCO_GRAVIDADE_grave'
+
+# Seleciona quais métodos de seleção de hiperparãmteros usar
+use_gridsearch = False
+use_optuna = False
+
+# Para df reduzido
+frac = 0.1  # Fração do dataset original
 #=====================================================================================
 
 
@@ -18,35 +25,32 @@ num_meses_previsao = 6
 with open(file_path, "r") as f:
     config = yaml.safe_load(f)
 
-# Carrega dados
-df_tipos = pd.read_csv('Modelos/data/df_tipos.csv')
-df_nacional_ambar = df_tipos[(df_tipos['TIPO_DEMANDA']=='NACIONAL') & (df_tipos['DESC_COR']=='AMBAR')].copy()
-df_nacional_ambar['QUANTIDADE_M'] = df_nacional_ambar['QUANTIDADE'] / 1_000_000
+# Carrega dados completos
+# URL dos dados a serem recuperados
+URL_DADOS_LIMPOS = "https://github.com/iz-120/ECM514-Projeto02-Assistente-Diagnostico/releases/download/v1.0-dados/dengue_limpo_2025.zip"
+# Nome exato do arquivo CSV dentro do .zip
+CSV_LIMPO = "dengue_limpo_2025.csv"
+# Chama a função para carregar os dados limpos
+df_dengue = carregar_dados_dengue_de_release(
+    url_release=URL_DADOS_LIMPOS,
+    ficheiro_csv_no_zip=CSV_LIMPO
+)
+df_dengue = df_dengue.dropna()
 
-# Seleciona top produtos
-data_corte = pd.to_datetime('2024-01-01')
-df_top_produtos_periodo = df_nacional_ambar[pd.to_datetime(df_nacional_ambar['DATA']) >= data_corte].groupby('PREFIXO')['QUANTIDADE'].sum().reset_index()
-df_top_produtos_periodo = df_top_produtos_periodo.sort_values(by='QUANTIDADE', ascending=False).reset_index(drop=True)
-top_produtos_periodo = df_top_produtos_periodo.head(5)['PREFIXO'].tolist()
+# Cria df reduzido
+df_dengue_reduzido, _ = train_test_split(df_dengue, train_size=frac, stratify=df_dengue['RISCO_GRAVIDADE_grave'], random_state=42)
 
-#==============================INPUT==================================================
-# Seleciona apenas as features e o target
-features = ['DATA', 'PREFIXO']
-target = 'QUANTIDADE'
-all = features.copy()
-all.append(target)
-#=====================================================================================
+# Verificar proporções
+print("Original:")
+print(df_dengue['RISCO_GRAVIDADE_grave'].value_counts(normalize=True))
+print("Reduzido:")
+print(df_dengue_reduzido['RISCO_GRAVIDADE_grave'].value_counts(normalize=True))
 
-# Cria df apenas com features e target
-df_feat_targ = df_nacional_ambar[all].copy()
+# Roda os experimentos
+if use_gridsearch:
+    # Roda experimentos com GridSearch
+    _, _, _ = treinar_gridsearch(df_dengue_reduzido, target, config)
 
-# Cria df_dict
-df_dict = criar_df_dict(df_feat_targ, top_produtos_periodo)
-
-# Roda experimentos individuais com GridSearch
-# for produto, (df_produto, datas) in df_dict.items():
-#     _, _, _ = treinar_produto(df_produto, target, datas, produto, config)
-
-# Roda experimentos individuais com Optuna
-for produto, (df_produto, datas) in df_dict.items():
-    _, _, _ = treinar_produto_optuna(df_produto, target, datas, produto, config, data_inicio, num_meses_previsao)
+if use_optuna:
+    # Roda experimentos com Optuna
+    _, _, _ = treinar_optuna(df_dengue_reduzido, target, config)
