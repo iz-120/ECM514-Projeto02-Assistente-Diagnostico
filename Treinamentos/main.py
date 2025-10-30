5bceb176bf0d0a9f318ba2de8fab0fd4234bd019
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2025-10-29 21:32:23 -0300

    Centraliza mais os inputs

--- Treinamentos/main.py
+++ Treinamentos/main.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import yaml
 from sklearn.model_selection import train_test_split
-# from src.train import treinar, treinar_optnua
+from src.train import treinar_gridsearch, treinar_optuna
 from src.utils import carregar_dados_dengue_de_release
 
 #==============================INPUT==================================================
+# Configurações gerais
+init = {
+    'project_name': 'Assistente_Diagnostico_Dengue',
+    'tags': ['teste'],
+    'name': 'Dengue_v1',
+    'df_name': 'df_dengue_reduzido_20'
+}
+
 # Seleciona experimento
 yaml_file = "xgb_grid_1"
 file_path = "Treinamentos/Experimentos/" + yaml_file + ".yaml"
@@ -13,11 +21,9 @@ file_path = "Treinamentos/Experimentos/" + yaml_file + ".yaml"
 target = 'RISCO_GRAVIDADE_grave'
 
 # Seleciona quais métodos de seleção de hiperparãmteros usar
-use_gridsearch = False
+use_gridsearch = True
 use_optuna = False
 
-# Para df reduzido
-frac = 0.1  # Fração do dataset original
 #=====================================================================================
 
 
@@ -25,26 +31,17 @@ frac = 0.1  # Fração do dataset original
 with open(file_path, "r") as f:
     config = yaml.safe_load(f)
 
-# Carrega dados completos
-# URL dos dados a serem recuperados
-URL_DADOS_LIMPOS = "https://github.com/iz-120/ECM514-Projeto02-Assistente-Diagnostico/releases/download/v1.0-dados/dengue_limpo_2025.zip"
-# Nome exato do arquivo CSV dentro do .zip
-CSV_LIMPO = "dengue_limpo_2025.csv"
-# Chama a função para carregar os dados limpos
-df_dengue = carregar_dados_dengue_de_release(
-    url_release=URL_DADOS_LIMPOS,
-    ficheiro_csv_no_zip=CSV_LIMPO
-)
-df_dengue = df_dengue.dropna()
-
-# Cria df reduzido
+# Carrega df reduzido
 df_dengue_reduzido = pd.read_csv('Treinamentos/data/df_dengue_reduzido_20.csv')
 
-# # Roda os experimentos
-# if use_gridsearch:
-#     # Roda experimentos com GridSearch
-#     _, _, _ = treinar_gridsearch(df_dengue_reduzido, target, config)
+# VERIFICAR MELHOR SCORING PARA APLICA_PARAMETROS EM MODELS
+# REVER PLOTS DE UTILS
 
-# if use_optuna:
-#     # Roda experimentos com Optuna
-#     _, _, _ = treinar_optuna(df_dengue_reduzido, target, config)
+# Roda os experimentos
+if use_gridsearch:
+    # Roda experimentos com GridSearch
+    _, _, _ = treinar_gridsearch(df_dengue_reduzido, target, config, init)
+
+if use_optuna:
+    # Roda experimentos com Optuna
+    _, _, _ = treinar_optuna(df_dengue_reduzido, target, config, init)
